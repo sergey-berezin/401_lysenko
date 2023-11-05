12015b2fe1ca0965eb2dd6fa2de04e9bf7a84983
Vitalii Fuglaev <86838952+HelenBylk@users.noreply.github.com>
2023-11-05 17:40:50 +0300

    Rewrite main task with use WhenAny()

--- ObjectDetectorApp/ObjectDetectorApp/Program.cs
+++ ObjectDetectorApp/ObjectDetectorApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using ONNXObjectDetector;
 
-namespace YOLO_csharp
+namespace ObjectDetectorApp
 {
     class Program
     {
         static ObjectDetectionService objectDetectionService = new();
+        static SemaphoreSlim outputFileLock = new(1, 1);
         static async Task Main(string[] args)
         {
             if (!File.Exists("results.csv"))
@@ -18,17 +19,31 @@ namespace YOLO_csharp
             }
 
             using var tokenSource = new CancellationTokenSource();
-            var tasks = args.Select(imagePath => objectDetectionService.RunAsync(imagePath, tokenSource.Token)).ToArray();
-            try
+            var tasks = args.Select(imagePath => objectDetectionService.RunAsync(imagePath, tokenSource.Token)).ToList();
+            var image_paths = args.ToList();
+            while (true)
             {
-                await Task.WhenAll(tasks);
-                foreach (var task in tasks)
-                    File.AppendAllLines("results.csv", task.Result.Select(obj => obj.ToCSVRow()));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message, e.StackTrace);
-                return;
+                if (!tasks.Any())
+                    break;
+
+                try
+                {
+                    var completed_task = await Task.WhenAny(tasks);
+                    string task_image_path = image_paths[tasks.IndexOf(completed_task)];
+                    tasks.Remove(completed_task);
+                    image_paths.Remove(task_image_path);
+
+                    await outputFileLock.WaitAsync();
+                    File.AppendAllLines("results.csv", completed_task.Result.Select(obj => obj.ToCSVRow()));
+                    Console.WriteLine($"Successful {task_image_path}");
+
+                    outputFileLock.Release();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message, e.StackTrace);
+                    outputFileLock.Release();
+                }
             }
         }
     }
